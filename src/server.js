6c5a2c6965a719d6d7aa4bf901752ad211a8bbae
Diego Rojas <diegorojas431@gmail.com>
2020-08-14 18:09:45 -0500

    Creando una aplicación de Express

--- src/server.js
+++ src/server.js
@@ -1,15 +1,3 @@
-# React router
-
-### Creando una aplicación de Express
-
-Vamos a crear un servidor básico con Express, para ello primero debemos instalarlo con el siguiente comando:
-
-`npm install express -S -E`
-
-Vamos a realizar algo llamado Backend for Frontend, de momento solo va a responder ante cualquier dirección con un texto que será una plantilla base de nuestro proyecto.
-
-./src/srver.js
-```
 const express = require('express');
 
 const app = express();
@@ -37,4 +25,3 @@ app.get('*', (req, res) => {
 app.listen(3000)
 
 console.log(`El servidor se prendió en el puerto 3000`);
-```
