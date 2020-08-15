3fb3e35e9104c9c6597e5d9654d8119cabca1709
Diego Rojas <diegorojas431@gmail.com>
2020-08-15 08:14:09 -0500

    ReactDOMServer

--- src/server.js
+++ src/server.js
@@ -1,27 +1,40 @@
 const express = require('express');
-const App = require('../dist/ssr/app')
-const ReactRouter = require('react-router')
+import React from 'react';
+const App = require('../dist/ssr/app');
+const { StaticRouter } = require('react-router');
+import reactDOMServer from 'react-dom/server';
 
 const app = express();
 
-<ReactRouter.StaticRotuter>
-	<App/>
-</ReactRouter.StaticRotuter>
+app.use(express.static('dist'));
+app.use('images', express.static('images'));
 
 app.get('*', (req, res) => {
 	console.log(req.url)
+	const html = reactDOMServer.renderToString(
+		<StaticRouter
+			location={req.url}
+			context={{
+				name: 'leonidas'
+			}}
+		>
+			<App/>
+		</StaticRouter>
+	)
 
 	res.write(`
 		<!DOCTYPE html>
 		<html lang="es">
 			<head>
 				<meta charset="UTF-8">
-				<title>PLatzi Video</title>
+				<link rel=stylesheet" href="/css/app.css">
+				<title>Platzi Video</title>
 			</head>
 			<body>
-				<div id="home-container"> Hola mundo! desde ${req.url}</div>
+				<div id="home-container">${html}</div>
 				<div id="modal-container"></div>
-				<script src="http://localhost:9000/js/app.js"></script>
+				<!--<script src="http://localhost:9000/js/app.js"></script> -->
+				<script src="/js/app.js"></script>
 			</body>
 		</html>
 	`)
